b88e8135a4293cf44d5d62335c315b375f48fe12
Richard <52008509+richardbr-coder@users.noreply.github.com>
2020-10-29 14:53:23 -0230

    Add files via upload

---
+++ MovieFinder/MovieFinder/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace MovieFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.directorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.releaseYearDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.priceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.moviesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.moviesDataSet = new MovieFinder.MoviesDataSet();
+            this.moviesTableAdapter = new MovieFinder.MoviesDataSetTableAdapters.MoviesTableAdapter();
+            this.displayTextBox = new System.Windows.Forms.ListBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.titleRadioButton = new System.Windows.Forms.RadioButton();
+            this.directorRadioButton = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moviesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moviesDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.titleDataGridViewTextBoxColumn,
+            this.directorDataGridViewTextBoxColumn,
+            this.releaseYearDataGridViewTextBoxColumn,
+            this.priceDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.moviesBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(752, 66);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(437, 313);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // titleDataGridViewTextBoxColumn
+            // 
+            this.titleDataGridViewTextBoxColumn.DataPropertyName = "Title";
+            this.titleDataGridViewTextBoxColumn.HeaderText = "Title";
+            this.titleDataGridViewTextBoxColumn.Name = "titleDataGridViewTextBoxColumn";
+            // 
+            // directorDataGridViewTextBoxColumn
+            // 
+            this.directorDataGridViewTextBoxColumn.DataPropertyName = "Director";
+            this.directorDataGridViewTextBoxColumn.HeaderText = "Director";
+            this.directorDataGridViewTextBoxColumn.Name = "directorDataGridViewTextBoxColumn";
+            // 
+            // releaseYearDataGridViewTextBoxColumn
+            // 
+            this.releaseYearDataGridViewTextBoxColumn.DataPropertyName = "ReleaseYear";
+            this.releaseYearDataGridViewTextBoxColumn.HeaderText = "ReleaseYear";
+            this.releaseYearDataGridViewTextBoxColumn.Name = "releaseYearDataGridViewTextBoxColumn";
+            // 
+            // priceDataGridViewTextBoxColumn
+            // 
+            this.priceDataGridViewTextBoxColumn.DataPropertyName = "Price";
+            this.priceDataGridViewTextBoxColumn.HeaderText = "Price";
+            this.priceDataGridViewTextBoxColumn.Name = "priceDataGridViewTextBoxColumn";
+            // 
+            // moviesBindingSource
+            // 
+            this.moviesBindingSource.DataMember = "Movies";
+            this.moviesBindingSource.DataSource = this.moviesDataSet;
+            // 
+            // moviesDataSet
+            // 
+            this.moviesDataSet.DataSetName = "MoviesDataSet";
+            this.moviesDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // moviesTableAdapter
+            // 
+            this.moviesTableAdapter.ClearBeforeFill = true;
+            // 
+            // displayTextBox
+            // 
+            this.displayTextBox.FormattingEnabled = true;
+            this.displayTextBox.Location = new System.Drawing.Point(66, 197);
+            this.displayTextBox.Name = "displayTextBox";
+            this.displayTextBox.Size = new System.Drawing.Size(319, 251);
+            this.displayTextBox.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(157, 152);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(136, 39);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(216, 91);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(152, 20);
+            this.inputTextBox.TabIndex = 3;
+            // 
+            // titleRadioButton
+            // 
+            this.titleRadioButton.AutoSize = true;
+            this.titleRadioButton.Location = new System.Drawing.Point(66, 80);
+            this.titleRadioButton.Name = "titleRadioButton";
+            this.titleRadioButton.Size = new System.Drawing.Size(97, 17);
+            this.titleRadioButton.TabIndex = 4;
+            this.titleRadioButton.TabStop = true;
+            this.titleRadioButton.Text = "Search By Title";
+            this.titleRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // directorRadioButton
+            // 
+            this.directorRadioButton.AutoSize = true;
+            this.directorRadioButton.Location = new System.Drawing.Point(66, 103);
+            this.directorRadioButton.Name = "directorRadioButton";
+            this.directorRadioButton.Size = new System.Drawing.Size(114, 17);
+            this.directorRadioButton.TabIndex = 5;
+            this.directorRadioButton.TabStop = true;
+            this.directorRadioButton.Text = "Search By Director";
+            this.directorRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(86, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter part of the title or director name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 491);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.directorRadioButton);
+            this.Controls.Add(this.titleRadioButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.displayTextBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moviesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moviesDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private MoviesDataSet moviesDataSet;
+        private System.Windows.Forms.BindingSource moviesBindingSource;
+        private MoviesDataSetTableAdapters.MoviesTableAdapter moviesTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn titleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn directorDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn releaseYearDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn priceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.ListBox displayTextBox;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.RadioButton titleRadioButton;
+        private System.Windows.Forms.RadioButton directorRadioButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MovieFinder/MovieFinder/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieFinder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'moviesDataSet.Movies' table. You can move, or remove it, as needed.
+            this.moviesTableAdapter.Fill(this.moviesDataSet.Movies);
+
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            displayTextBox.Items.Clear();
+            string userInput = inputTextBox.Text.ToLower();
+            if (titleRadioButton.Checked)
+            {
+                displayTextBox.Items.Add("The titles that have " + "''" + inputTextBox.Text + "''" + " in them are:");
+                displayTextBox.Items.Add("\n");
+
+                this.moviesTableAdapter.Fill(this.moviesDataSet.Movies);
+                var title = from m in this.moviesDataSet.Movies
+                                 where m.Title.IndexOf(userInput, StringComparison.OrdinalIgnoreCase) >= 0
+                                 select m;
+                foreach (var m in title)
+                {
+                    displayTextBox.Items.Add(m.Title);
+
+                }
+                
+            }
+            else if (directorRadioButton.Checked)
+            {
+                this.moviesTableAdapter.Fill
+                    (this.moviesDataSet.Movies);
+                var director = from d in this.moviesDataSet.Movies
+                                 where d.Director.IndexOf(userInput, StringComparison.OrdinalIgnoreCase) >= 0
+                                 select d;
+
+                displayTextBox.Items.Add("The directors name is: " + inputTextBox.Text);
+                displayTextBox.Items.Add("\n");
+                displayTextBox.Items.Add(inputTextBox.Text + " directed such classics as: ");
+                foreach (var d in director)
+                {
+                    displayTextBox.Items.Add(d.Title);
+                }
+            }
+        }
+    }
+}
